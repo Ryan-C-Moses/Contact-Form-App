5ce51d5d48e5b47a44169cffc040180f79e3d20f
rhythmos <ryancmoses@outlook.com>
2025-01-11 01:07:42 -0500

    CFA-1 added more dependencies and updated test script

--- test/e2e/pa11yTest.js
+++ test/e2e/pa11yTest.js
@@ -1,9 +1,28 @@
-const pa11y = require('pa11y');
-const fs = require('fs');
+const puppeteer = require("puppeteer");
+const pa11y = require("pa11y");
+const fs = require("fs");
+
+const webpage = "https://ryan-c-moses.github.io/Contact-Form-App/";
 
 (async () => {
-    const results = await pa11y('https://ryan-c-moses.github.io/Contact-Form-App/');
-    fs.writeFileSync('reports/pa11y-accessibility-report.json', JSON.stringify(results, null, 2));
-    console.log(results.issues);
-    console.log('\n\n\n\nReport saved to accessibility-report.json');
+  const browser = await puppeteer.launch({
+    headless: true, // Run in headless mode
+    args: ["--no-sandbox", "--disable-setuid-sandbox"], // Add the no-sandbox flag
+  });
+
+  const page = await browser.newPage();
+  await page.goto(webpage);
+  await test(webpage);
+
+  await browser.close();
 })();
+
+const test = async (webpage) => {
+  const results = await pa11y(webpage);
+  fs.writeFileSync(
+    "reports/pa11y-accessibility-report.json",
+    JSON.stringify(results, null, 2)
+  );
+  console.log(results.issues);
+  console.log("\n\n\n\nReport saved to accessibility-report.json");
+};
