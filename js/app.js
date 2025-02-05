25f948a779679050bfa62a8acfc4d8bfeefa0987
rhythmos <ryancmoses@outlook.com>
2025-02-05 00:24:15 -0500

    CFA-25 Add directory changes 	- re-organizing code 	- working through error validation

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import validateForm from './form-validation.js';
+
+// Ensure the DOM is fully loaded before setting up form validation
+document.addEventListener('DOMContentLoaded', () => {
+  // Call the validateForm function to set up form validation
+  validateForm();
+});
