beadee2a3b29619f422f595b4be2b26b78eaa13b
rhythmos <ryancmoses@outlook.com>
2025-06-04 10:02:00 -0400

    CFA-6 complete modal integration

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 import validateForm from './form-validation.js';
 
+// variables
+const form = document.getElementById("contact-form-id");
+
 // Ensure the DOM is fully loaded before setting up form validation
 document.addEventListener('DOMContentLoaded', () => {
   // Call the validateForm function to set up form validation
-  validateForm();
+  // validateForm();
 });
+
+form.addEventListener("submit", validateForm)
