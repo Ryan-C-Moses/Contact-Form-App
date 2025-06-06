207619f07c938cc8b83f00e51aac4d631e42aad1
rhythmos <ryancmoses@outlook.com>
2025-06-05 23:34:06 -0400

    CFA-34 cleaned up validation onsubmit logic

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,33 +1,53 @@
-// Initial Logic to Validate the Form
+// Gather form elements
+const fname = document.getElementById("first-name");
+const lname = document.getElementById("last-name");
+const email = document.getElementById("email");
+const queryType = document.getElementById("query-type");
+const message = document.getElementById("message");
+const consent = document.getElementById("consent");
+const generalEnquiry = document.getElementById("general-enquiry");
+const supportRequest = document.getElementById("support-request");
+const [querytype1, querytype2] = queryType.children[1].children;
+
+const fNameErr = document.getElementById("fname-error");
+const lNameErr = document.getElementById("lname-error");
+const emailErr = document.getElementById("email-error");
+
+const nameRegex = /^[A-Za-z]+$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const registerEventListeners = () => {
+  fname.addEventListener("input", (evt) =>
+    validate(nameRegex, fname, fNameErr)
+  );
+  lname.addEventListener("input", (evt) =>
+    validate(nameRegex, lname, lNameErr)
+  );
+  email.addEventListener("input", (evt) =>
+    validate(emailRegex, email, emailErr)
+  );
+  message.addEventListener("input", (evt) => {
+    const input = evt.target.value;
+    if (input < 1) {
+      document.getElementById("message-error").style.display = "block";
+      el.classList.add("error-input");
+    }
+  });
+};
 
 const validateForm = (event) => {
   // Prevent the default form submission
   event.preventDefault();
 
-  // Gather form elements
-  const fname = document.getElementById("first-name");
-  const lname = document.getElementById("last-name");
-  const email = document.getElementById("email");
-  const queryType = document.getElementById("query-type");
-  const message = document.getElementById("message");
-  const consent = document.getElementById("consent");
-
   // Validate form fields
-  validateFirstNameChange(fname);
-  validateLastNameChange(lname);
-  validateEmailChange(email);
-  validateQueryTypeChange(queryType);
-  validateMessageChange(message);
-  validateConsentChange(consent);
+  validateFirstNameChange();
+  validateLastNameChange();
+  validateEmailChange();
+  validateQueryTypeChange();
+  validateMessageChange();
+  validateConsentChange();
 
-  const checkForm = isFormValid(
-    fname,
-    lname,
-    email,
-    queryType,
-    message,
-    consent
-  );
+  const checkForm = isFormValid(fname, lname, email, message, consent);
 
   // Check if the form is valid before submitting
   if (checkForm) {
@@ -39,7 +59,7 @@ const validateForm = (event) => {
   console.log("form submitted");
 };
 
-const isFormValid = (fname, lname, email, queryType, message, consent) => {
+const isFormValid = (fname, lname, email, message, consent) => {
   const generalEnquiry = document.getElementById("general-enquiry");
   const supportRequest = document.getElementById("support-request");
 
@@ -80,76 +100,41 @@ const hideSuccessMessage = () => {
   }, 4000);
 };
 
-const validateFirstNameChange = (el) => {
-  const regex = /^[A-Za-z]+$/;
-  const err = document.getElementById("fname-error");
-
-  el.addEventListener("input", () => validate(regex, el, err));
-  validate(regex, el, err); // Initial validation check
+const validateFirstNameChange = () => {
+  validate(nameRegex, fname, fNameErr);
 };
 
-const validateLastNameChange = (el) => {
-  const regex = /^[A-Za-z]+$/;
-  const err = document.getElementById("lname-error");
-
-  el.addEventListener("input", () => validate(regex, el, err));
-  validate(regex, el, err); // Initial validation check
+const validateLastNameChange = () => {
+  validate(nameRegex, lname, lNameErr);
 };
 
-const validateEmailChange = (el) => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const err = document.getElementById("email-error");
-
-  const validateEmail = (e) => {
-    if (regex.test(e.target.value)) {
-      err.style.display = "none";
-      el.classList.remove("error-input");
-    } else {
-      err.style.display = "block";
-      el.classList.add("error-input");
-    }
-  };
-
-  el.addEventListener("change", validateEmail);
-  validate(regex, el, err); // Initial validation check
+const validateEmailChange = () => {
+  validate(emailRegex, email, emailErr);
 };
 
-const validateQueryTypeChange = (el) => {
-  const generalEnquiry = document.getElementById("general-enquiry");
-  const supportRequest = document.getElementById("support-request");
-
-  const div1 = el.children[1];
-  const div2 = el.children[2];
-
+const validateQueryTypeChange = () => {
   if (!(generalEnquiry.checked || supportRequest.checked)) {
     document.getElementById("query-type-error").style.display = "block";
-    div1.classList.add("error-input");
-    div2.classList.add("error-input");
+    querytype1.classList.add("error-input");
+    querytype2.classList.add("error-input");
   }
 };
 
-const validateMessageChange = (el) => {
-  if (el.value.length < 1) {
+const validateMessageChange = () => {
+  if (message.value.length < 1) {
     document.getElementById("message-error").style.display = "block";
-    el.classList.add("error-input");
+    message.classList.add("error-input");
   }
-
-  el.addEventListener("input", (evt) => {
-    const input = evt.target.value;
-    if (input < 1) {
-      document.getElementById("message-error").style.display = "block";
-      el.classList.add("error-input");
-    }
-  });
 };
 
-const validateConsentChange = (el) => {
-  if (!el.checked) {
+const validateConsentChange = () => {
+  if (!consent.checked) {
     document.getElementById("consent-error").style.display = "block";
   }
 };
 
 const validate = (regex, el, err) => {
+  console.log(el.value);
   if (regex.test(el.value)) {
     err.style.display = "none";
     el.classList.remove("error-input");
